f5f4a882d51738c2387e46fc6c44994548842846
unknown <anejjar1324@gmail.com>
2024-02-22 14:26:06 +0100

    First React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+
+const hedy = {
+  name : 'hedy lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  size : 130,
+}
+
+
+function AboutPage() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1 id="daTitle">The ugliest Web page On Github XD</h1>
+      <h1>About</h1>
+      <h2>Wanna learn about {hedy.name} ?</h2>
+      <p>Kifach la .. zid gallak .<br /> 
+      that's her .. look how pretty she is (*was)..</p>
+
+      <img 
+        src={hedy.imageUrl} 
+        style = {{
+            width:hedy.size,
+            height:hedy.size
+        }}></img>
+      <p>Safi yallah lay3aawn</p>
+      <MyBtn/>
+    </>
   );
 }
 
-export default App;
+function MyBtn(){
+  return (
+      <>
+        <p id="miw"> Why so serious ? malk mamrtahch </p>
+        <button id="hada"> brek hna  </button>
+      </>
+  );
+}
+
+
+export  default AboutPage;
+
+
+
+
+
